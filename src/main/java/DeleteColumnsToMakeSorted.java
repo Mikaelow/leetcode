92ef7109f6918a1c35b63fa94c7581a8fd025898
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-01-04 18:05:24 +0100

    Working with sort nad reverseSort

--- src/main/java/DeleteColumnsToMakeSorted.java
+++ src/main/java/DeleteColumnsToMakeSorted.java
@@ -1,18 +1,36 @@
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.List;
 
 class DeleteColumnsToMakeSorted {
     public int minDeletionSize(String[] strs){
-        int count=0;
-        String sortedStrs;
-        for (int i=0;i< strs.length;i++){
-             sortedStrs= Arrays.toString(strs[i]).replace('[',"");
-            System.out.println(sortedStrs);
-           /* if (strs[i].equals(sortedStrs)) {
-                count++;
-            }*/
+        int count=0,size= strs.length;
+        List<String> sortedStrs = new ArrayList<>();
+
+
+        while(count<strs.length) {
+            Arrays.stream(strs[count].split("")).sorted().forEach(sortedStrs::add);
+            String sortedSt=sortedStrs.toString();
+            StringBuilder reversedSorted= new StringBuilder();
+
+            sortedSt=sortedSt.replace("[","");
+            sortedSt=sortedSt.replace(",","");
+            sortedSt=sortedSt.replace("]","");
+            sortedSt=sortedSt.replace(" ","");
+
+            String[] reserve=sortedSt.split("");
+            for (int i=sortedSt.length();i>0;i--)
+                reversedSorted.append(reserve[i-1]);
+            System.out.println(reversedSorted);
+           // ------------------------------------------------------------------------------------------
+            if (strs[count].equals(sortedSt)
+                    ||
+                    strs[count].equals(reversedSorted.toString()))
+                size--;
+            count++;
+            sortedStrs=new ArrayList<>();
         }
 
-    return count;
+    return size;
     }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,7 @@
 class main {
     public static void main(String[] args) {
         DeleteColumnsToMakeSorted soultion= new DeleteColumnsToMakeSorted();
-        int cod=soultion.minDeletionSize(new String[]{"cba","daf","ghi"});
+        int cod=soultion.minDeletionSize(new String[]{"zyx","wvu","tsr"});
         System.out.println(cod);
     }
     }
