7b5bdc67819ac713ea65c5d6a034a62869c657c9
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-01-24 22:08:25 +0100

    mergeTwoLists basic

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,40 +1,5 @@
 class Solution {
-    public boolean isValid(String s) {
-        char[] arrayChar=s.toCharArray();
-        if (s.equals("[({])}")) //XDDDDDDDDDDDDDDDDDDDDDDDDDD
-            return false;
-        for (int i=0;i< arrayChar.length;i++)
-        {
-            if ((int) arrayChar[i]==40) {
-                for (int j = i + 1; j < arrayChar.length; j++)
-                    if ((int) arrayChar[j] == 41 && (j-i-1)%2==0) {
-                        arrayChar[j] = 0;
-                        arrayChar[i]=0;
-                        break;
-                    }
-
-            }
-            else if ((int) arrayChar[i]==91) {
-                        for (int j = i + 1; j < arrayChar.length; j++)
-                            if ((int) arrayChar[j] == 93 && (j-i-1)%2==0) {
-                                arrayChar[j] = 0;
-                                arrayChar[i] = 0;
-                                break;
-                            }
-                    }
-            else if ((int) arrayChar[i]==123) {
-                for (int j = i + 1; j < arrayChar.length; j++)
-                    if ((int) arrayChar[j] == 125 && (j-i-1)%2==0) {
-                        arrayChar[j] = 0;
-                        arrayChar[i] = 0;
-                        break;
-
-                    }
-            }
-        }
-        for (int i=0;i< arrayChar.length;i++)
-            if (arrayChar[i]!=0)
-                return false;
-        return true;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    return new ListNode();
     }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,9 @@
 class main {
     public static void main(String[] args) {
+
+
         Solution soultion= new Solution();
-        boolean cod=soultion.isValid("[({])}");
+        ListNode cod=soultion.mergeTwoLists(new ListNode(123),new ListNode(234));
         System.out.println(cod);
 
     }
