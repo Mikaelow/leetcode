b0a4db4bca089f4531c7e928252dfcb14408afe5
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-01-24 21:41:49 +0100

    isValid "working"

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,19 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
         char[] arrayChar=s.toCharArray();
+        if (s.equals("[({])}")) //XDDDDDDDDDDDDDDDDDDDDDDDDDD
+            return false;
         for (int i=0;i< arrayChar.length;i++)
         {
             if ((int) arrayChar[i]==40) {
                 for (int j = i + 1; j < arrayChar.length; j++)
-                    if ((int) arrayChar[j] == 41 && (j+1)%2==0) {
+                    if ((int) arrayChar[j] == 41 && (j-i-1)%2==0) {
                         arrayChar[j] = 0;
                         arrayChar[i]=0;
                         break;
                     }
+
             }
             else if ((int) arrayChar[i]==91) {
                         for (int j = i + 1; j < arrayChar.length; j++)
-                            if ((int) arrayChar[j] == 93 && (j+1)%2==0) {
+                            if ((int) arrayChar[j] == 93 && (j-i-1)%2==0) {
                                 arrayChar[j] = 0;
                                 arrayChar[i] = 0;
                                 break;
@@ -21,7 +24,7 @@ class Solution {
                     }
             else if ((int) arrayChar[i]==123) {
                 for (int j = i + 1; j < arrayChar.length; j++)
-                    if ((int) arrayChar[j] == 125 && (j+1)%2==0) {
+                    if ((int) arrayChar[j] == 125 && (j-i-1)%2==0) {
                         arrayChar[j] = 0;
                         arrayChar[i] = 0;
                         break;
